8536029a728c349ef25bbaadefabfafe2b4fb954
gorfadumesh <gorfadumesh1213@gmail.com>
2020-12-09 12:04:09 +0530

    form add

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -8,6 +8,13 @@ function TodoForm({ addTodo }) {
     task: "",
     completed: false,
   });
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
 
   function handleTaskInputChange(e) {
     // e.target.value contains new input from onChange
@@ -18,6 +25,16 @@ function TodoForm({ addTodo }) {
   function handleSubmit(e) {
     e.preventDefault(); // prevents browser refresh
     // trim() gets rid of string whitespace
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...todo }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+
     if (todo.task.trim()) {
       addTodo({ ...todo, id: uuid() });
       setTodo({ ...todo, task: "" });
@@ -25,26 +42,15 @@ function TodoForm({ addTodo }) {
   }
 
   return (
-    <form name="contact" method="post">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message"></textarea>
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <TextField
+        label="Task"
+        type="text"
+        name="task"
+        value={todo.task}
+        onChange={handleTaskInputChange}
+      />
+      <Button type="submit">Submit</Button>
     </form>
   );
 }
